9d247c03975d871cc51d01a2c521882b8e266b99
Paula Gearon <pgearon@imohealth.com>
2023-08-02 16:23:37 -0400

    Fixed IRI syntax error for XSD time types

--- test/donatello/ttl_test.clj
+++ test/donatello/ttl_test.clj
@@ -20,9 +20,9 @@
     (is (= "xsd:long" (serialize :xsd/long)))
     (is (= "<http://test.org/>" (serialize (URL. "http://test.org/"))))
     (is (= "<http://test.org/>" (serialize (URI. "http://test.org/"))))
-    (is (= "\"2023-02-11T22:39:06.109Z\"^^<xsd:dateTime>" (serialize (Date. 1676155146109))))
-    (is (= "\"2023-02-11T22:39:06.109Z\"^^<xsd:dateTime>" (serialize (Instant/ofEpochMilli 1676155146109))))
-    (is (= "\"2023-02-11\"^^<xsd:date>" (serialize (LocalDate/of 2023 2 11))))
+    (is (= "\"2023-02-11T22:39:06.109Z\"^^xsd:dateTime" (serialize (Date. 1676155146109))))
+    (is (= "\"2023-02-11T22:39:06.109Z\"^^xsd:dateTime" (serialize (Instant/ofEpochMilli 1676155146109))))
+    (is (= "\"2023-02-11\"^^xsd:date" (serialize (LocalDate/of 2023 2 11))))
     (is (= "\"-26.84372,150.54195\"^^asami:geo" (serialize (ttl/typed-literal "-26.84372,150.54195" :asami/geo))))
     (is (= "\"-26.84372,150.54195\"^^<http://quoll.clojars.org/geo>"
            (serialize (ttl/typed-literal "-26.84372,150.54195" (URL. "http://quoll.clojars.org/geo")))))
